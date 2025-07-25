375c8e35b586d0ef5c53f2f5940537c311d4e667
24yn0108 <24yn0108@jec.ac.jp>
2025-07-25 18:21:15 +0900

    Azure SQL用にconfig.phpを修正

--- helpers/config.php
+++ helpers/config.php
@@ -1,5 +1,23 @@
 <?php
-    // DB接続設定
-    define('DSN', 'sqlsrv:server=10.32.97.1\Web;database=24yn0108');
-    define('DB_USER', '24yn0108');
-    define('DB_PASSWORD', '24yn0108');
+// DB接続設定
+define('DB_SERVER', 'tcp:24yn0108db.database.windows.net,1433');
+define('DB_DATABASE', '24yn0108');
+define('DB_USER', '24yn0108');
+define('DB_PASSWORD', 'Pa$$word1234'); // ← 設定したパスワードに書き換え
+
+// 接続処理
+$connectionOptions = array(
+    "Database" => DB_DATABASE,
+    "Uid" => DB_USER,
+    "PWD" => DB_PASSWORD,
+    "Encrypt" => true,
+    "TrustServerCertificate" => false
+);
+
+$conn = sqlsrv_connect(DB_SERVER, $connectionOptions);
+
+// 接続チェック
+if ($conn === false) {
+    die(print_r(sqlsrv_errors(), true));
+}
+?>
