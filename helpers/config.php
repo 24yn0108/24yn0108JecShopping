6dc80c39570eca0d749f2193159e5244091b83c7
24yn0108 <24yn0108@jec.ac.jp>
2025-07-25 18:34:18 +0900

    定数修正：DB_SERVERとDB_DATABASEを定義

--- helpers/config.php
+++ helpers/config.php
@@ -1,9 +1,9 @@
 <?php
 // DB接続設定
 define('DB_SERVER', 'tcp:24yn0108db.database.windows.net,1433');
-define('DB_DATABASE', '24yn0108');
+define('DB_DATABASE', '24yn0108'); // ← データベース名
 define('DB_USER', '24yn0108');
-define('DB_PASSWORD', 'Pa$$word1234'); // ← 設定したパスワードに書き換え
+define('DB_PASSWORD', 'Pa$$word1234'); // ← 正しいパスワードを設定
 
 // 接続処理
 $connectionOptions = array(
